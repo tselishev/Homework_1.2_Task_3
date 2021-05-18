58daf0c8b339471f4dfdfa623851213a4d20cb3e
tselishev <82764755+tselishev@users.noreply.github.com>
2021-05-18 20:47:27 +0500

    Изм. решение

--- src/main/kotlin/ru/netology/task3.kt
+++ src/main/kotlin/ru/netology/task3.kt
@@ -2,21 +2,25 @@ package ru.netology
 
 fun main() {
 
-    val result = calc(1000.00, 10500.00, "meloman")
-    println("сумма к оплате: $result")
+    val resultToDouble = calc(1000.00, 10500.00, "meloman")
+    val result = String.format("%.2f", resultToDouble)
+    println("сумма к оплате: $result руб")
 }
 
 fun calc(totalProd: Double, historyProd: Double, status: String = "normal"): Double {
     var totalCalc = 0.00
-    when {
-        historyProd >= 1001.00 && historyProd < 10000.00 -> totalCalc = totalProd - 100.00
-        historyProd >= 10001.00 -> totalCalc = totalProd / 1.05
-        historyProd < 1000.00 -> totalCalc = totalProd
+    when { // '* 100' -переводим в копейки
+        historyProd * 100 >= 1001.00 && historyProd * 100 < 10000.00 -> totalCalc = totalProd * 100 - 100.00
+        historyProd * 100 >= 10001.00 -> totalCalc = totalProd * 100 / 1.05
+        historyProd * 100 < 1000.00 -> totalCalc = totalProd * 100
     }
-    when (status){
-        "meloman" -> totalCalc /= 1.05
+    if (status == "meloman") {
+        totalCalc /= 1.05
+    } else {
+        totalCalc
     }
-    return totalCalc
+    return totalCalc / 100  // переводим обратно в руб
+
 }
 
 
